d85e408aa82c334262b441cf67b508dbc2a489e5
Sathvik K S <sathvik@perspectiv.in>
2023-05-03 09:44:32 +0530

    updated app.py to use new model

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ IMG_SIZE = 224
 def top_2_accuracy(in_gt, in_pred):
     return top_k_categorical_accuracy(in_gt, in_pred, k=2)
 
-retina_model = load_model('./model/densenet_10_4_2023_drs.h5') 
+retina_model = load_model('model\densenet_1_10_4_2023.h5') 
 graph = tf.compat.v1.get_default_graph()
 
 @app.route('/')
@@ -27,7 +27,7 @@ def index():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict_image():
     if request.method == 'POST':
-        predictions=["Mild","Moderate","NO_DR","Proliferate_DR","Severe"] 
+        predictions=["Mild","Moderate","No_DR","Proliferate_DR","Severe"] 
         message = request.get_json(force=True)
         encoded = message['image']
         decoded = base64.b64decode(encoded)
@@ -37,10 +37,7 @@ def predict_image():
         img = np.expand_dims(np_image, axis=0)
         predict=retina_model.predict(img)
         preds = predict[0]
-        print(preds)
         pred=np.argmax(predict,axis=1)
-        print(pred)
-        print(f"Predicted: {predictions[pred[0]]}")
         res = [(predictions[k], round(100*v, 2)) for k, v in sorted(enumerate(preds), key = lambda x: -1*x[1])]
         del(img)
         print(res)
